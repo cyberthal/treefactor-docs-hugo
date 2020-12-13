2a6f57b122b9c14c95e04d36ed7aab26a7adbd46
leo3 <texas.cyberthal@gmail.com>
2020-12-14 04:48:10 +0800

    track upload script

---
+++ Upload.sh
@@ -0,0 +1,1 @@
+rsync -irvzh --delete --force --progress --exclude=".DS-Store" /Users/leo3/1-Mansort/3-Binary/Pubmind/Hugo/Treefactor/Prod/public/ leo3_treefactor-docs@ssh.phx.nearlyfreespeech.net:/home/public/
